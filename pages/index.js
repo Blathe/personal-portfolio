2707c5c42b24352eadd304cdd6fc53a6fd69d0b3
Scott Peterts <Scottpeters2281@gmail.com>
2022-06-05 19:13:04 -0700

    removing unused api folder/files

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container-fluid">
+      <div>
         <Header />
-        <main className="container-fluid">
+        <main>
           <HeroSection />
           <AboutSection />
           <ProjectsSection />
