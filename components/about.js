234d42cd5b2f50673ddc0ea78f48145bfa0d6d11
Scott Peterts <Scottpeters2281@gmail.com>
2022-09-11 13:13:47 -0700

    some small changes to About Me section

--- components/about.js
+++ components/about.js
@@ -15,19 +15,11 @@ const AboutSection = () => {
             Currently enrolled in Western Governor&apos;s University working
             towards my Bachelor&apos;s in Software Development with a focus on
             C# (although I&apos;m starting to enjoy JavaScript) - expected
-            graduation is late 2022/early 2023. I am the founder of{" "}
-            <span className="text-emerald-800 font-bold underline dark:text-emerald-500">
-              <Link href="https://digitalemberstudio.com">
-                Digital Ember Studio
-              </Link>
-            </span>{" "}
-            , a local web development studio that helps small businesses with
-            their web presense.
+            graduation is late 2022/early 2023.
           </p>
           <p className="text-lg 2xl:text-xl font-mons font-normal tracking-wide">
-            In the little spare time I have, I enjoy photography, learning about
-            new technologies, the occasional MMO, and messing with the Unity
-            game engine.
+            In the little spare time I have, I enjoy photography, fly fishing, and reading up on
+            new technologies.
           </p>
         </div>
 
