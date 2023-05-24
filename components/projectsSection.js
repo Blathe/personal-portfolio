c0d4a0b17dc0a84b80539bf27659b35d95c7328d
Scott Peters <scottpeters2281@gmail.com>
2023-05-23 21:19:15 -0700

    adding new project to portfolio, fixing some UI stuff.

--- components/projectsSection.js
+++ components/projectsSection.js
@@ -10,8 +10,31 @@ const ProjectsSection = () => {
         </h1>
 
         <div className="grid grid-cols-1 gap-20 xl:gap-8 xl:grid-cols-3 mx-auto">
+        <div className="w-100">
+            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0 flex flex-col gap-4 h-full">
+              <div className="outline outline-4 outline-emerald-800 overflow-hidden grow-0">
+                <img
+                  className="hover:scale-125 transition duration-750"
+                  src="dashboard.jpg"
+                  alt=""
+                  width="900"
+                  height="553"
+                />
+              </div>
+              <div className="mt-8 flex gap-4 flex-col font-mons grow">
+                <h1 className="text-lg font-bold">Ticket Management System (Capstone)</h1>
+                <p>
+                  This is my capstone project for my Software Engineering degree. The application is written in Python with the Django framework. It uses a PostgreSQL database and is deployed to Heroku.
+                </p>
+                <p>
+                  <span className="font-bold">Technologies:</span> Python/Django, Bootstrap 5, PostgreSQL, Heroku
+                </p>
+              </div>
+              <a className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-md px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-full" href="https://djangotickets.herokuapp.com/" target="_blank" rel="noreferrer">View Deployed Application</a>
+            </div>
+          </div>
           <div className="w-100">
-            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0">
+            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0 flex flex-col gap-4 h-full">
               <div className="outline outline-4 outline-emerald-800 overflow-hidden">
                 <img
                   className="hover:scale-125 transition duration-750"
@@ -21,7 +44,7 @@ const ProjectsSection = () => {
                   height="553"
                 />
               </div>
-              <div className="mt-8 flex flex-col gap-4 font-mons">
+              <div className="mt-8 flex flex-col gap-4 font-mons grow">
                 <h1 className="text-lg font-bold">
                   Digital Ember Studio - Small Business Website
                 </h1>
@@ -33,14 +56,12 @@ const ProjectsSection = () => {
                   <span className="font-bold">Technologies:</span> Next.js, Tailwind
                   CSS, Figma, Adobe Illustrator, Netlify
                 </p>
-                <div className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-56">
-                  <a href="https://digitalemberstudio.com" target="_blank" rel="noreferrer">View Live Site</a>
-                </div>
               </div>
+              <a href="https://digitalemberstudio.com" target="_blank" rel="noreferrer" className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-full">View Live Site</a>
             </div>
           </div>
-          <div className="w-100">
-            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0">
+          <div className="w-100 h-full">
+            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0 flex flex-col gap-4 h-full">
               <div className="outline outline-4 outline-emerald-800 overflow-hidden">
                 <img
                   className="hover:scale-125 transition duration-750"
@@ -50,43 +71,18 @@ const ProjectsSection = () => {
                   height="553"
                 />
               </div>
-              <div className="mt-8 flex flex-col gap-4 font-mons">
-                <h1 className="text-lg font-bold">Tesla Clone</h1>
-                <p>
-                  This is a clone of Tesla&apos;s website using Next.js, and Tailwind CSS - deployed to Netlify.
-                </p>
-                <p>
-                  <span className="font-bold">Technologies:</span> Next.js, Tailwind CSS, Netlify
-                </p>
-                <div className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-56">
-                  <a href="https://teslaclone123.netlify.app" target="_blank" rel="noreferrer">View Live Site</a>
+              <div className="mt-8 flex flex-col gap-4 font-mons grow">
+                <div className="flex flex-col grow gap-4">
+                  <h1 className="text-lg font-bold">Tesla Clone</h1>
+                  <p>
+                    This is a clone of Tesla&apos;s website using Next.js, and Tailwind CSS - deployed to Netlify.
+                  </p>
+                  <p>
+                    <span className="font-bold">Technologies:</span> Next.js, Tailwind CSS, Netlify
+                  </p>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="w-100">
-            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0">
-              <div className="outline outline-4 outline-emerald-800 overflow-hidden">
-                <img
-                  className="hover:scale-125 transition duration-750"
-                  src="coming-soon.webp"
-                  alt=""
-                  width="900"
-                  height="553"
-                />
-              </div>
-              <div className="mt-8 flex flex-col gap-4 font-mons">
-                <h1 className="text-lg font-bold">Coming Soon...</h1>
-                <p>
-                  This project is currently a work in progress, once it is at an acceptable stage to show it will be displayed.
-                </p>
-                {/*<p>
-                  <span className="font-bold">Technologies:</span>{" "}
-                </p>
-                <div className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-56">
-                  <a href="#" target="_blank">Coming Soon</a>
-                </div>*/}
-              </div>
+              <a href="https://teslaclone123.netlify.app" target="_blank" rel="noreferrer" className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-full">View Live Site</a>
             </div>
           </div>
         </div>
