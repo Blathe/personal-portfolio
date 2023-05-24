7fda5112b4b42e374643c781591a5a63f1c46771
Scott Peters <scottpeters2281@gmail.com>
2023-05-23 17:15:51 -0700

    updating copyright

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="anim-fade-in text-center font-mons dark:text-white">
       <p className="text-light font-sm p-4">
-        Copyright &copy; 2022 -{" "}
+        Copyright &copy; 2023 -{" "}
         <span className="text-emerald-700 font-bold dark:text-emerald-500">
           Scott Peters
         </span>
