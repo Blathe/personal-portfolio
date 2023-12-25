813bc02d278dfc6e55f0fff4629178d183ee80d3
Scott Peters <scottpeters2281@gmail.com>
2023-12-24 21:48:46 -0800

    updated projects section

--- components/projectsSection.js
+++ components/projectsSection.js
@@ -17,6 +17,32 @@ const ProjectsSection = () => {
             <div className="px-8 lg:px-36 xl:px-8 2xl:px-0 flex flex-col gap-4 h-full">
               <div className="outline outline-4 outline-emerald-800 overflow-hidden">
                 <img
+                  className="hover:scale-125 transition duration-750"
+                  src="codecapture.jpg"
+                  alt=""
+                  width="900"
+                  height="553"
+                />
+              </div>
+              <div className="mt-8 flex flex-col gap-4 font-mons grow">
+                <h1 className="text-lg font-bold">
+                  CodeCapture.io
+                </h1>
+                <p>
+                  CodeCapture.io is a tool to quickly generate beautiful images of code snippets.
+                </p>
+                <p>
+                  <span className="font-bold">Technologies:</span> Next.js, Tailwind
+                  CSS, Vercel
+                </p>
+              </div>
+              <a href="https://codecapture.io" target="_blank" className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-full">View Live Site</a>
+            </div>
+          </div>
+          <div className="w-100">
+            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0 flex flex-col gap-4 h-full">
+              <div className="outline outline-4 outline-emerald-800 overflow-hidden">
+                <img
                   className="hover:scale-125 transition duration-750 position-cover"
                   src="dashboard.jpg"
                   alt="A colorful dashboard"
@@ -37,33 +63,6 @@ const ProjectsSection = () => {
                 </p>
               </div>
               <a className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-md px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-full" href="https://djangotickets.herokuapp.com/" target="_blank" rel="noreferrer">View Deployed Application</a>
-            </div>
-          </div>
-          <div className="w-100">
-            <div className="px-8 lg:px-36 xl:px-8 2xl:px-0 flex flex-col gap-4 h-full">
-              <div className="outline outline-4 outline-emerald-800 overflow-hidden">
-                <img
-                  className="hover:scale-125 transition duration-750 object-none"
-                  src="digital-ember.webp"
-                  alt=""
-                  width="900"
-                  height="553"
-                />
-              </div>
-              <div className="mt-8 flex flex-col gap-4 font-mons grow">
-                <h1 className="text-lg font-bold">
-                  Digital Ember Studio - Small Business Website
-                </h1>
-                <p>
-                  Digital Ember Studio is a website built using
-                  NextJS and Tailwind CSS. I designed a mockup of the website in Figma, and did the logo in Adobe Illustrator.
-                </p>
-                <p>
-                  <span className="font-bold">Technologies:</span> Next.js, Tailwind
-                  CSS, Figma, Adobe Illustrator, Netlify
-                </p>
-              </div>
-              <a href="https://digitalemberstudio.com" target="_blank" rel="noreferrer" className="text-center shadow-md font-mons text-white bg-emerald-700 font-medium rounded-md text-xl px-10 py-2.5 hover:scale-105 hover:shadow-lg transition w-full">View Live Site</a>
             </div>
           </div>
           <div className="w-100 h-full">
